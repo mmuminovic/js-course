0b067f556cc434f0c9d42b425cdca631bd346028
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2022-10-14 21:17:35 +0200

    if - else /  boolean data type

--- 2  - JS numbers and strings/script.js
+++ 2  - JS numbers and strings/script.js
@@ -20,6 +20,25 @@ zbir = prviBroj + drugiBroj; // 11
 document.write(zbir);
 */
 
+// Boolean - tip podataka
+// var uslov = true;
+// var drugiUslov = false;
+
+// if (uslov) {
+//   console.log("Ovo se desilo");
+// } else {
+//   console.log("Zadati uslov nije zadovoljen");
+// }
+
+// var prviSabirak = 1;
+// var drugiSabirak = 2;
+
+// if (prviSabirak > drugiSabirak) {
+//   console.log("Prvi sabirak je veci");
+// } else {
+//   console.log("Prvi sabirak nije veci");
+// }
+
 var a, b, rezultat;
 
 // SABERI
@@ -28,14 +47,71 @@ document.getElementById("saberiButton").addEventListener("click", function () {
   b = +document.getElementById("drugiBroj").value;
   rezultat = a + b;
   document.getElementById("rezultat").textContent = rezultat;
+  if (rezultat < 0) {
+    document.getElementById("rezultat").style =
+      "background-color: red; color: white;";
+  } else if (rezultat === 0) {
+    document.getElementById("rezultat").style =
+      "background-color: orange; color: white";
+  } else {
+    document.getElementById("rezultat").style =
+      "background-color: green; color: white;";
+  }
 });
 
 // ODUZMI
 document.getElementById("oduzmiButton").addEventListener("click", function () {
-  a = +document.getElementById("prviBroj").value;
-  b = +document.getElementById("drugiBroj").value;
+  a = document.getElementById("prviBroj").value; // string
+  b = document.getElementById("drugiBroj").value; // string
+
+  // if (a === "" || b === "") {
+  //   alert("Proverite vasa polja, neko od njih je prazno");
+  //   return;
+  // }
+
+  a = Number(a); // pretvaramo u broj
+  b = Number(b); // pretvaramo u broj
+
+  // var obaBrojaSuPozitivna = a > 0 && b > 0;
+
+  // var nisuObaBrojaPozitivna = a <= 0 || b <= 0;
+  // nisuObaBrojaPozitivna === !obaBrojaSuPozitivna
+
+  // if (!obaBrojaSuPozitivna) {
+  //   console.log("Nisu oba broja pozitivna!");
+  // }
+
   rezultat = a - b;
   document.getElementById("rezultat").textContent = rezultat;
+  if (rezultat < 0) {
+    document.getElementById("rezultat").style =
+      "background-color: red; color: white;";
+  } else if (rezultat === 0) {
+    document.getElementById("rezultat").style =
+      "background-color: orange; color: white";
+  } else {
+    document.getElementById("rezultat").style =
+      "background-color: green; color: white;";
+  }
+  // if (rezultat > 0) {
+  //   console.log("Rezultat je veci od nule");
+  // }
+
+  // if (rezultat > 1) {
+  //   console.log("Rezultat je veci od 1");
+  // }
+
+  // if (rezultat > 2) {
+  //   console.log("Rezultat je veci od 2");
+  // }
+
+  // if (rezultat > 0) {
+  //   console.log("Rezultat je veci od nule");
+  // } else if (rezultat > 1) {
+  //   console.log("Rezultat je veci od 1");
+  // } else if (rezultat > 2) {
+  //   console.log("Rezultat je veci od 2");
+  // }
 });
 
 // PODELI
@@ -44,6 +120,16 @@ document.getElementById("podeliButton").addEventListener("click", function () {
   b = +document.getElementById("drugiBroj").value;
   rezultat = a / b;
   document.getElementById("rezultat").textContent = rezultat;
+  if (rezultat < 0) {
+    document.getElementById("rezultat").style =
+      "background-color: red; color: white;";
+  } else if (rezultat === 0) {
+    document.getElementById("rezultat").style =
+      "background-color: orange; color: white";
+  } else {
+    document.getElementById("rezultat").style =
+      "background-color: green; color: white;";
+  }
 });
 
 // POMNOZI
@@ -52,4 +138,14 @@ document.getElementById("pomnoziButton").addEventListener("click", function () {
   b = +document.getElementById("drugiBroj").value;
   rezultat = a * b;
   document.getElementById("rezultat").textContent = rezultat;
+  if (rezultat < 0) {
+    document.getElementById("rezultat").style =
+      "background-color: red; color: white;";
+  } else if (rezultat === 0) {
+    document.getElementById("rezultat").style =
+      "background-color: orange; color: white";
+  } else {
+    document.getElementById("rezultat").style =
+      "background-color: green; color: white;";
+  }
 });
