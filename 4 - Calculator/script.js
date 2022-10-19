5cb478408ce92361db9ed3833c47a47a7b94a9ee
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2022-10-19 20:38:11 +0200

    swich

--- 4 - Calculator/script.js
+++ 4 - Calculator/script.js
@@ -1,50 +1,67 @@
 var finalValue; // Rezultat
 // finalValue === undefined
-var currentValue = 0; // Number s kojim cemo da radimo (sabiranje mnozenje itd)
+var currentValue = 0; // Broj koji je trenutni broj kojeg unosimo na digitronu
 var op = ""; // Operacija koju smo izabrali
 
 document.getElementById("one").addEventListener("click", function () {
   currentValue = currentValue * 10 + 1;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
 document.getElementById("two").addEventListener("click", function () {
   currentValue = currentValue * 10 + 2;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
 document.getElementById("three").addEventListener("click", function () {
   currentValue = currentValue * 10 + 3;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
 document.getElementById("four").addEventListener("click", function () {
   currentValue = currentValue * 10 + 4;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
 document.getElementById("five").addEventListener("click", function () {
   currentValue = currentValue * 10 + 5;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
 document.getElementById("six").addEventListener("click", function () {
   currentValue = currentValue * 10 + 6;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
@@ -54,33 +71,45 @@ document.getElementById("six").addEventListener("click", function () {
 
 document.getElementById("seven").addEventListener("click", function () {
   currentValue = currentValue * 10 + 7;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
 document.getElementById("eight").addEventListener("click", function () {
   currentValue = currentValue * 10 + 8;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
 document.getElementById("nine").addEventListener("click", function () {
   currentValue = currentValue * 10 + 9;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
 document.getElementById("zero").addEventListener("click", function () {
   currentValue = currentValue * 10 + 0;
+
+  console.log("------------------------");
   console.log("finalValue", finalValue);
   console.log("currentValue", currentValue);
   console.log("op", op);
+
   document.getElementById("result").textContent = currentValue;
 });
 
@@ -94,48 +123,72 @@ document.getElementById("delete").addEventListener("click", function () {
 });
 
 document.getElementById("equal").addEventListener("click", function () {
-  if (op === "") {
-    finalValue = currentValue;
-  } else if (op === "+") {
-    finalValue = finalValue + currentValue;
-  } else if (op === "-") {
-    finalValue = finalValue - currentValue;
-  } else if (op === "*") {
-    finalValue = finalValue * currentValue;
-  } else if (op === "/") {
-    finalValue = finalValue / currentValue;
+  switch (op) {
+    case "":
+      finalValue = currentValue;
+    case "+":
+      finalValue = finalValue + currentValue;
+      break;
+    case "-":
+      finalValue = finalValue - currentValue;
+      break;
+    case "*":
+      finalValue = finalValue * currentValue;
+      break;
+    case "/":
+      finalValue = finalValue / currentValue;
+      break;
+    default:
+      break;
   }
 
+  console.log("------------------------");
   console.log("op", op);
   console.log("finalValue on EQUAL", finalValue);
+
   document.getElementById("result").textContent = finalValue;
 
   //
   console.log("==== Vracamo varijable na pocetne vrednosti ====");
+
   op = "";
+
   console.log("inital op", op);
+
   currentValue = 0;
   finalValue = undefined;
+
   console.log("inital finalValue", finalValue);
   console.log("initial currentValue", currentValue);
 });
 
 document.getElementById("plus").addEventListener("click", function () {
-  if (op === "") {
-    finalValue = currentValue;
-  } else if (op === "+") {
-    finalValue = finalValue + currentValue;
-  } else if (op === "-") {
-    finalValue = finalValue - currentValue;
-  } else if (op === "*") {
-    finalValue = finalValue * currentValue;
-  } else if (op === "/") {
-    finalValue = finalValue / currentValue;
+  switch (op) {
+    case "":
+      finalValue = currentValue;
+    case "+":
+      finalValue = finalValue + currentValue;
+      break;
+    case "-":
+      finalValue = finalValue - currentValue;
+      break;
+    case "*":
+      finalValue = finalValue * currentValue;
+      break;
+    case "/":
+      finalValue = finalValue / currentValue;
+      break;
+    default:
+      break;
   }
 
+  console.log("------------------------");
   console.log("op", op);
+
   op = "+";
+
   console.log("new OP", op);
+
   currentValue = 0;
   document.getElementById("result").textContent = "";
 
@@ -144,21 +197,32 @@ document.getElementById("plus").addEventListener("click", function () {
 });
 
 document.getElementById("minus").addEventListener("click", function () {
-  if (op === "") {
-    finalValue = currentValue;
-  } else if (op === "+") {
-    finalValue = finalValue + currentValue;
-  } else if (op === "-") {
-    finalValue = finalValue - currentValue;
-  } else if (op === "*") {
-    finalValue = finalValue * currentValue;
-  } else if (op === "/") {
-    finalValue = finalValue / currentValue;
+  switch (op) {
+    case "":
+      finalValue = currentValue;
+    case "+":
+      finalValue = finalValue + currentValue;
+      break;
+    case "-":
+      finalValue = finalValue - currentValue;
+      break;
+    case "*":
+      finalValue = finalValue * currentValue;
+      break;
+    case "/":
+      finalValue = finalValue / currentValue;
+      break;
+    default:
+      break;
   }
 
+  console.log("------------------------");
   console.log("op", op);
+
   op = "-";
+
   console.log("new OP", op);
+
   currentValue = 0;
   document.getElementById("result").textContent = "";
 
@@ -167,21 +231,32 @@ document.getElementById("minus").addEventListener("click", function () {
 });
 
 document.getElementById("times").addEventListener("click", function () {
-  if (op === "") {
-    finalValue = currentValue;
-  } else if (op === "+") {
-    finalValue = finalValue + currentValue;
-  } else if (op === "-") {
-    finalValue = finalValue - currentValue;
-  } else if (op === "*") {
-    finalValue = finalValue * currentValue;
-  } else if (op === "/") {
-    finalValue = finalValue / currentValue;
+  switch (op) {
+    case "":
+      finalValue = currentValue;
+    case "+":
+      finalValue = finalValue + currentValue;
+      break;
+    case "-":
+      finalValue = finalValue - currentValue;
+      break;
+    case "*":
+      finalValue = finalValue * currentValue;
+      break;
+    case "/":
+      finalValue = finalValue / currentValue;
+      break;
+    default:
+      break;
   }
 
+  console.log("------------------------");
   console.log("op", op);
+
   op = "*";
+
   console.log("new OP", op);
+
   currentValue = 0;
   document.getElementById("result").textContent = "";
 
@@ -190,21 +265,32 @@ document.getElementById("times").addEventListener("click", function () {
 });
 
 document.getElementById("divide").addEventListener("click", function () {
-  if (op === "") {
-    finalValue = currentValue;
-  } else if (op === "+") {
-    finalValue = finalValue + currentValue;
-  } else if (op === "-") {
-    finalValue = finalValue - currentValue;
-  } else if (op === "*") {
-    finalValue = finalValue * currentValue;
-  } else if (op === "/") {
-    finalValue = finalValue / currentValue;
+  switch (op) {
+    case "":
+      finalValue = currentValue;
+    case "+":
+      finalValue = finalValue + currentValue;
+      break;
+    case "-":
+      finalValue = finalValue - currentValue;
+      break;
+    case "*":
+      finalValue = finalValue * currentValue;
+      break;
+    case "/":
+      finalValue = finalValue / currentValue;
+      break;
+    default:
+      break;
   }
 
+  console.log("------------------------");
   console.log("op", op);
+
   op = "/";
+
   console.log("new OP", op);
+
   currentValue = 0;
   document.getElementById("result").textContent = "";
 
