816a1faec492b6923d85c8d2a4606397a3747634
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2022-10-24 21:01:08 +0200

    for loop

---
+++ 7 - Loops/script.js
@@ -0,0 +1,65 @@
+// var i;
+// for (i = 0; i < 3; i++) { // i++ <=> i=i+1;
+//   console.log("Hello world");
+// }
+
+// var j = 0;
+
+// if (j < 3) {
+//   console.log("Hello world");
+//   j++;
+// }
+
+// if (j < 3) {
+//   console.log("Hello world");
+//   j++;
+// }
+
+// if (j < 3) {
+//   console.log("Hello world");
+//   j++;
+// }
+
+// var zbir = 0;
+// var i;
+// for (i = 1; i <= 10; i++) {
+//   zbir = zbir + i;
+// }
+
+// console.log("Zbir je jednak broju: ", zbir);
+
+// var a = 5;
+// var ostatakPriDeljenjuSaDva = 5 % 2; // 1
+
+// Napisati funkciju koja ce ispisati u konzoli
+// brojeve od 1 do 20 koji su deljivi sa 3
+
+// var num;
+// for (num = 20; num > 0; num--) {
+//   if (num % 3 === 0) {
+//     console.log(num);
+//   }
+// }
+
+// Ispitati sve parne brojeve od 2 do 20
+// i ispisati one deljive sa 3
+
+var k;
+for (k = 1; k <= 20; k++) {
+  if (k % 3 === 0 || k % 2 === 0) {
+    console.log(k);
+  }
+}
+
+// DOMACI: Napisati skriptu koja ce za dva uneta broja
+// ispitati sve te brojeve izmedju ta dva broja
+// ukljucujuci ta dva broja
+// i ispisati u konzoli sledece
+// - ako je broj deljiv sa 3 "fizz"
+// - ako je broj deljiv sa 5 "buzz"
+// - ako je broj deljiv i sa 3 i sa 5 "fizz buzz"
+
+// na HTML stranici ispisati koliko puta se ispisao "fizz"
+// koliko puta "buzz", a koliko puta "fizz buzz"
+
+// for(i=a; i<=b; i++)
