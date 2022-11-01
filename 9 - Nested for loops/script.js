a71ef16f524cbc46124225e4270ff8aefc761d19
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2022-11-01 08:32:41 +0100

    tablica mnozenja

---
+++ 9 - Nested for loops/script.js
@@ -0,0 +1,19 @@
+// Napisati skriptu koja ce u konzoli ispisati tablicu mnozenja
+var i, j;
+
+var tablica = document.getElementById("tablica"); // html element
+for (i = 1; i <= 10; i++) {
+  var row = document.createElement("tr"); // html element koji smo kreirali
+  var th = document.createElement("th"); // html element koji cemo dodati u row
+  th.textContent = i;
+  row.appendChild(th);
+
+  for (j = 1; j <= 10; j++) {
+    var proizvod = i * j;
+    var td = document.createElement("td");
+    td.textContent = proizvod;
+    row.appendChild(td);
+  }
+
+  tablica.appendChild(row);
+}
