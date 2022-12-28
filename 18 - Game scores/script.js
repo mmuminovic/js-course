d5c509bf633220a7c3cf720148d36a915fb8dbab
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2022-12-28 19:50:32 +0100

    img fix

--- 18 - Game scores/script.js
+++ 18 - Game scores/script.js
@@ -3,37 +3,58 @@ var goals = [];
 var minutes = 0;
 var seconds = 0;
 
-setInterval(function(){
+setInterval(function () {
   seconds++;
-  if(seconds === 60){
+  if (seconds === 60) {
     minutes++;
     seconds = 0;
   }
   var minutesToShow = minutes;
   var secondsToShow = seconds;
 
-  if(seconds < 10){
-    secondsToShow = "0"+secondsToShow;
+  if (seconds < 10) {
+    secondsToShow = "0" + secondsToShow;
   }
 
-  if(minutes < 10){
-    minutesToShow = "0"+ minutesToShow;
+  if (minutes < 10) {
+    minutesToShow = "0" + minutesToShow;
   }
 
-  document.getElementById('time').textContent = minutesToShow + ":" + secondsToShow;
+  document.getElementById("time").textContent =
+    minutesToShow + ":" + secondsToShow;
 }, 1000);
 
 document.getElementById("addGoal").addEventListener("click", function () {
   // Uzimamo vrednosti
-  var min = minutes+1;
+  var min = minutes + 1;
   var player = document.getElementById("score").value;
   var team = document.getElementById("team").value;
+
+  var imgSrc;
+
+  var event = document.getElementById("event").value;
+
+  switch (event) {
+    case "yellowcard":
+      imgSrc =
+        "https://cdn1.vectorstock.com/i/1000x1000/99/90/soccer-referees-hand-with-yellow-card-vector-1169990.jpg";
+      break;
+    case "redcard":
+      imgSrc =
+        "https://media.istockphoto.com/id/157679439/photo/red-card.jpg?s=612x612&w=0&k=20&c=AfuyasC4B4W7qHqeZtkuwSesvW7S5Fl5LoclDvTR4VY=";
+      break;
+    default:
+      imgSrc =
+        "https://img.freepik.com/premium-vector/soccer-ball-icon-logo-template-football-logo-symbol_7649-4092.jpg?w=2000";
+      break;
+  }
 
   // Smestamo vrednosti u objekat kao propertiji
   var goal = {
     min: min,
     player: player,
     team: team,
+    img: imgSrc,
   };
 
   // Objekat saljemo u niz goals
@@ -45,53 +66,46 @@ document.getElementById("addGoal").addEventListener("click", function () {
   document.getElementById("team").value = "";
 
   // Parent ul html element
-  var listOfGoalsElement = document.getElementById('listOfGoals');
+  var listOfGoalsElement = document.getElementById("listOfGoals");
 
   // Izbrisemo sve sto je pre toga bilo u listi da bi opet ispocetka ubacili sve elemente
-  listOfGoalsElement.innerHTML = ""
+  listOfGoalsElement.innerHTML = "";
 
   var moroccoGoals = 0;
   var franceGoals = 0;
 
-  goals.forEach(function(item, index){
-    var goalElement = document.createElement('li');
-    var imgSrc;
+  goals.forEach(function (item, index) {
+    var goalElement = document.createElement("li");
 
-    var event = document.getElementById('event').value;
+    goalElement.innerHTML =
+      '<img src="' +
+      item.img +
+      '" width="20px">' +
+      item.min +
+      " - " +
+      item.player;
 
-    switch(event){
-      case 'yellowcard':
-        imgSrc = 'https://cdn1.vectorstock.com/i/1000x1000/99/90/soccer-referees-hand-with-yellow-card-vector-1169990.jpg'
-        break;
-      case 'redcard': 
-        imgSrc = 'https://media.istockphoto.com/id/157679439/photo/red-card.jpg?s=612x612&w=0&k=20&c=AfuyasC4B4W7qHqeZtkuwSesvW7S5Fl5LoclDvTR4VY=';
-        break;
-      default: 
-        imgSrc = 'https://img.freepik.com/premium-vector/soccer-ball-icon-logo-template-football-logo-symbol_7649-4092.jpg?w=2000';
-        break;
-    }
-
-    goalElement.innerHTML = '<img src="' + imgSrc + '" width="50px">' + item.min + ' - ' + item.player;
-
-    if(item.team === 'France'){
-        goalElement.style = "text-align: right;"
-        franceGoals++;
+    if (item.team === "France") {
+      goalElement.style =
+        "display:flex; align-items: center; justify-content: flex-end";
+      franceGoals++;
     } else {
-        moroccoGoals++;
+      goalElement.style = "display:flex; align-items: center;";
+      moroccoGoals++;
     }
 
     listOfGoalsElement.appendChild(goalElement);
   });
 
-  document.getElementById('goal-gif').style.display = "block";
-  
-  setTimeout(function(){
-    document.getElementById('goal-gif').style.display = "none";
+  document.getElementById("goal-gif").style.display = "block";
+
+  setTimeout(function () {
+    document.getElementById("goal-gif").style.display = "none";
   }, 3000); // milisekunde ==> 1 sekunda = 1000 milisekundi
 
   // Ispisujemo rezultat
-  document.getElementById('moroccoScore').textContent = moroccoGoals;
-  document.getElementById('franceScore').textContent = franceGoals;
+  document.getElementById("moroccoScore").textContent = moroccoGoals;
+  document.getElementById("franceScore").textContent = franceGoals;
 });
 
 // var colorInt = 255;
@@ -103,8 +117,8 @@ document.getElementById("addGoal").addEventListener("click", function () {
 //   if(colorInt > 0){
 //     colorInt--;
 //   }
-//   // rgb(255, 255, 255) 
-//   // rgb(254, 254, 254) 
+//   // rgb(255, 255, 255)
+//   // rgb(254, 254, 254)
 
 // }, 100);
 
