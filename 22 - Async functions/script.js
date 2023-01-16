4b01405a80c93d5c8f01b50520abb3d94af25691
Muhamed Muminović <muminovic.muhamed01@gmail.com>
2023-01-16 21:06:04 +0100

    fetch post method

--- 22 - Async functions/script.js
+++ 22 - Async functions/script.js
@@ -6,37 +6,62 @@ async function calculateSum(a, b) {
     // !isNaN(a) - a jeste broj
     // isNaN(a) => true or false
     if (!isNaN(a) && !isNaN(b)) {
-      resolve(a + b);
+      resolve({ a: a, b: b, sum: a + b });
     } else {
       reject("Uneti brojevi nisu ispravni");
     }
   });
 }
 
+// var result = calculateSum(2, 3);
 // calculateSum(2, 3).then().catch();
 // calculateSum(2, "a") ===> vraca error u catch bloku
-calculateSum(2, 3)
+calculateSum(2, 2)
   .then(function (result) {
-    /* 
-    u then se nalazi funkcija koja ce da handluje podatke 
+    /*
+    u then se nalazi funkcija koja ce da handluje podatke
     koje smo dobili nakon sto je promise uspesno zavrsen
     */
     // prikazi korisniku podatke koje je trazio
-    console.log("result", result);
+    // document.write(result.sum);
   })
   .catch(function (error) {
     // prikazi korisniku poruku da nesto nije u redu sa internetom
-    console.log("error", error);
+    // console.log("error", error);
+    // document.write(error);
   });
 
 // fetch - asinhrona funkcija koja nam sluzi da saljemo zahteve (requests) na server
-fetch("https://jsonplaceholder.typicode.com/posts/1")
+fetch("https://jsonplaceholder.typicode.com/posts/100")
   .then(function (result) {
     return result.json();
   })
-  .then(function (result) {
-    console.log("FETCH RESPONSE", result);
+  .then(function (result2) {
+    console.log("FETCH RESPONSE", result2);
   })
   .catch(function (error) {
     console.log("FETCH ERROR", error);
   });
+
+// default method = GET
+// other methods: POST, PUT, PATCH, DELETE
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST", // za kreiranje novih resursa
+  body: JSON.stringify({
+    title: "itcamp",
+    body: "Test test test",
+    userId: 999,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+// var user = {
+//   name: "Muhamed",
+// };
+
+// console.log(JSON.stringify(user));
