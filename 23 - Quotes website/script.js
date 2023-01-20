c4dc0ae0e8c35828f99d88b637dedd78831e1c6e
Muhamed Muminović <muminovic.muhamed01@gmail.com>
2023-01-20 20:56:59 +0100

    quotes search

--- 23 - Quotes website/script.js
+++ 23 - Quotes website/script.js
@@ -1,4 +1,5 @@
 var quotes = [];
+var allQuotes = [];
 
 function renderQuotes() {
   var parentEl = document.getElementById("list");
@@ -16,8 +17,21 @@ fetch("https://js-course-server.onrender.com/quotes/get-all-quotes")
   })
   .then(function (data) {
     quotes = data;
+    allQuotes = data;
     renderQuotes();
   })
   .catch(function (err) {
     console.log("err", err);
   });
+
+document.getElementById("search").addEventListener("keyup", function () {
+  var searchValue = document.getElementById("search").value;
+  quotes = allQuotes.filter(function (item, index) {
+    if (item.quoteText) {
+      return item.quoteText.toLowerCase().includes(searchValue.toLowerCase());
+    } else {
+      return false;
+    }
+  });
+  renderQuotes();
+});
