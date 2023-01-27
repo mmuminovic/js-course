d439051131d4a69b0c4333aef64e6b93844a23d0
Muhamed Muminović <muminovic.muhamed01@gmail.com>
2023-01-27 20:18:07 +0100

    signup page

---
+++ 23 - Quotes website/signup.js
@@ -0,0 +1,27 @@
+document.getElementById("login").addEventListener("click", function () {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
+  var fullName = document.getElementById("fullName").value;
+
+  var loginData = {
+    email: email,
+    password: password,
+    confirmPassword: confirmPassword,
+    fullName: fullName,
+  };
+
+  fetch("https://js-course-server.onrender.com/user/signup", {
+    method: "POST",
+    body: JSON.stringify(loginData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      alert(data.message);
+    });
+});
